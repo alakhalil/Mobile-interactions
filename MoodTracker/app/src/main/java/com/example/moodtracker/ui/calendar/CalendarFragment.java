d9f0c07fcff3f46cf4d6ae417a25356c4a741bfc
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-06-24 01:39:48 +0200

    add calendar to the bottom_nav_menu

--- MoodTracker/app/src/main/java/com/example/moodtracker/ui/calendar/CalendarFragment.java
+++ MoodTracker/app/src/main/java/com/example/moodtracker/ui/calendar/CalendarFragment.java
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.moodtracker.databinding.FragmentNotificationsBinding;
+import com.example.moodtracker.databinding.FragmentCalendarBinding;
 
-public class NotificationsFragment extends Fragment {
+public class CalendarFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentCalendarBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        CalendarViewModel calendarViewModel =
+                new ViewModelProvider(this).get(CalendarViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textCalendar;
+        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
