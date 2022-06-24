d0b3a02f75043e01f102c040372ec56013a6aa32
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-06-24 20:44:44 +0200

    add the list of entries

---
+++ MoodTracker/app/src/main/java/com/example/moodtracker/ui/home/Entry.java
@@ -0,0 +1,16 @@
+package com.example.moodtracker.ui.home;
+
+public class Entry {
+    String feeling_description, feeling_reason, entry_time;
+    int attached_image_id;
+    int feeling_emoji_id;
+
+
+    public Entry(String feeling_description, String feeling_reason, String entry_time, int attached_image_id, int feeling_emoji_id) {
+        this.feeling_description = feeling_description;
+        this.feeling_reason = feeling_reason;
+        this.entry_time = entry_time;
+        this.attached_image_id = attached_image_id;
+        this.feeling_emoji_id = feeling_emoji_id;
+    }
+}
--- MoodTracker/app/src/main/java/com/example/moodtracker/ui/home/HomeFragment.java
+++ MoodTracker/app/src/main/java/com/example/moodtracker/ui/home/HomeFragment.java
@@ -10,7 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.moodtracker.R;
 import com.example.moodtracker.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -23,6 +26,30 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        int[] imageId = {R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline,
+                R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline,R.drawable.emoticon_sad_outline};
+
+
+        String[] feeling_description = {"Feeling sad","Feeling happy", "Feeling happy", "Feeling sad", "Feeling happy", "Feeling depressed","Feeling depressed","Feeling depressed","Feeling depressed"};
+
+        String[] feeling_reason = {"had problems with my family","Supp","Let's Catchup","Dinner tonight?","Gotta go", "i'm in meeting","Gotcha","Let's Go","any Weekend Plans?"};
+
+        String[] entry_time = {"8:45 pm","9:00 am","7:34 pm","6:32 am","5:76 am", "5:00 am","7:34 pm","2:32 am","7:76 am"};
+
+        ArrayList<Entry> entryArrayList = new ArrayList<>();
+
+        for(int i = 0;i< imageId.length;i++){
+
+            Entry entry = new Entry(feeling_description[i],feeling_reason[i],entry_time[i],imageId[i], imageId[i]);
+            entryArrayList.add(entry);
+
+        }
+
+
+        ListAdapter listAdapter = new ListAdapter(getActivity(), entryArrayList);
+
+        binding.entries.setAdapter(listAdapter);
         return root;
     }
 
