98b20353deebc5b82cdd4c4257d831c7a71ea822
AlaaEzzeldin <khalila@fim.uni-passau.de>
2022-07-10 14:16:54 +0200

    make new date dynamically

--- MoodTracker/app/src/main/java/com/example/moodtracker/repositories/EntryRepository.java
+++ MoodTracker/app/src/main/java/com/example/moodtracker/repositories/EntryRepository.java
@@ -34,9 +34,8 @@ public class EntryRepository {
         int[] attachedImageId = {0, R.drawable.img, 0, 0, 0};
         String[] feeling_description = {"Feeling sad", "Feeling happy", "Feeling depressed", "Feeling great", "Feeling okay"};
         String[] feeling_reason = {"had problems with my family", "happy with my grades", "Stressed", "Will travel tonight", "Nothing special"};
-        String[] entry_time = {"8:45 pm", "9:00 am", "7:34 pm", "6:32 am", "5:76 am"};
         for (int i = 0; i < imageId.length; i++) {
-            Entry entry = new Entry(feeling_description[i], feeling_reason[i], entry_time[i], attachedImageId[i], imageId[i]);
+            Entry entry = new Entry(feeling_description[i], feeling_reason[i], attachedImageId[i], imageId[i]);
             dataset.add(entry);
         }
     }
